956e8dea9219559c5b704f9721ad74ffcfd8895e
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-10-23 20:32:59 +0330

    add RolePermissionSeeder seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Database\Seeders\RolePermissionSeeder as SeedersRolePermissionSeeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use RolePermissionSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            SeedersRolePermissionSeeder::class
+        ]);
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -12,45 +12,33 @@ class RolePermissionSeeder extends Seeder
 {
     public function run()
     {
-        // Create roles
         $adminRole = Role::create(['name' => 'admin']);
         $productManagerRole = Role::create(['name' => 'product_manager']);
         $productLeaderRole = Role::create(['name' => 'product_leader']);
 
-        // Define permissions
-        Permission::create(['name' => 'product.create']);
-        Permission::create(['name' => 'product.edit']);
-        Permission::create(['name' => 'product.update']);
-        Permission::create(['name' => 'product.view']);
-
-        // Assign permissions to roles
         $adminRole->givePermissionTo(['product.create', 'product.edit', 'product.update', 'product.view']);
         $productManagerRole->givePermissionTo(['product.view']);
         $productLeaderRole->givePermissionTo(['product.view']);
 
-        // Create admin user
         $admin = User::create([
             'name' => 'Admin User',
             'email' => 'admin@gmail.com',
-            'password' => '123456789', // Default password
+            'password' => '123456789',
         ]);
         $admin->assignRole($adminRole);
 
-        // Create product manager user
         $productManager = User::create([
             'name' => 'Product Manager',
             'email' => 'manager@gmail.com',
-            'password' => '123456789', // Default password
+            'password' => '123456789',
         ]);
         $productManager->assignRole($productManagerRole);
 
-        // Create product leader user
         $productLeader = User::create([
             'name' => 'Product Leader',
             'email' => 'leader@gmail.com',
-            'password' => '123456789', // Default password
+            'password' => '123456789',
         ]);
         $productLeader->assignRole($productLeaderRole);
     }
 }
-
