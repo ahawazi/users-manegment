62a4ec796fc5397821e9d4f7d0d17d8f26d5a27c
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-10-23 23:01:40 +0330

    add HasPanelShield, HasRoles in model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Spatie\Permission\Traits\HasRoles;
+use BezhanSalleh\FilamentShield\Traits\HasPanelShield;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Product extends Model implements FilamentUser
 {
+    use  HasRoles, HasPanelShield;
     protected $fillable = ['name', 'results', 'product_manager_id', 'product_leader_id'];
 
     public function productManager()
